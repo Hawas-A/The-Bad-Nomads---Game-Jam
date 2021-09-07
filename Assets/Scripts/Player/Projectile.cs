d1222cfc7c059d406db9a332d114369eaf5c49e9
Hawas-Adel <hawas@thebadnomads.com>
2021-09-07 11:50:35 +0200

    Player Score

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -11,4 +11,13 @@ public class Projectile : MonoBehaviour
 		GetComponent<Rigidbody>().velocity = transform.forward * Speed;
 		Destroy(gameObject, LifeTime);
 	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Enemy enemy = other.GetComponentInParent<Enemy>();
+		if (enemy)
+		{
+			enemy.OnHit();
+		}
+	}
 }
