5c5bf95f82615b41326188ea6e017219df3ee955
Hawas-Adel <hawas@thebadnomads.com>
2021-09-07 11:00:47 +0200

    Removed Githup Package

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -1,18 +1,20 @@
 using NaughtyAttributes;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour
 {
+	[SerializeField] [Min(1)] private int SpawnRateRange = new Vector2(0.5f, 3f);
 	[SerializeField] [MinMaxSlider(0, 10)] private Vector2 SpawnRateRange = new Vector2(0.5f, 3f);
 
-	private Enemy[] EnemyPrefaps;
+	private Enemy[] Enemies;
 	[System.NonSerialized] public float SpawnRateMult = 1;
 
 	private void Awake()
 	{
-		EnemyPrefaps = FindObjectsOfType<Enemy>(true);
-		System.Array.ForEach(EnemyPrefaps, E => E.gameObject.SetActive(false));
+		Enemies = FindObjectsOfType<Enemy>(true);
+		System.Array.ForEach(Enemies, E => E.gameObject.SetActive(false));
 	}
 
 	private IEnumerator Start()
@@ -23,7 +25,13 @@ public class SpawnEnemies : MonoBehaviour
 			RandomDirection.z = 0;
 			RandomDirection.Normalize();
 
-			//var Disabled
+			for (int i = 0; i < length; i++)
+			{
+
+			}
+			var DisabledEnemies = Enemies.Where(E => !E.gameObject.activeSelf);
+			var enemy1 = DisabledEnemies.ElementAt(Random.Range(0, DisabledEnemies.Count()));
+			enemy1.gameObject.SetActive(true);
 
 			yield return new WaitForSeconds(SpawnRateMult * Random.Range(SpawnRateRange.x, SpawnRateRange.y));
 		}
