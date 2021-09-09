0610bf6005fa2689f9b7ba5de1e09aea5aec6fd0
Hawas-Adel <hawas@thebadnomads.com>
2021-09-09 08:57:17 +0200

    camera changes

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,5 +1,7 @@
+using NaughtyAttributes;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
@@ -8,11 +10,18 @@ public class GameOverUI : MonoBehaviour
 
 	[SerializeField] private TextMeshProUGUI ScoreText;
 
+	[SerializeField] [Scene] private string MainMenuScene;
+
 	private void OnEnable()
 	{
 		System.Array.ForEach(DisabledObjects, O => O.SetActive(false));
 		System.Array.ForEach(DisabledBehaviours, B => B.enabled = false);
 
 		ScoreText.text = FindObjectOfType<PlayerScore>().Score.ToString();
+	}
+
+	public void ToMainMenu()
+	{
+		SceneManager.LoadScene(MainMenuScene);
 	}
 }
